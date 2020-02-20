0746d4d9fefe26e359281d8692f076e716d1d5fc
ccs96307 <ccs96307@gmail.com>
2020-02-20 23:34:26 +0800

    2020/02/20 Update

--- Desktop Tool/main.py
+++ Desktop Tool/main.py
@@ -4,6 +4,7 @@ import re
 
 from PyQt5 import QtWidgets
 from UI import Ui_MainWindow
+from highlight import Highlight
 
 from pygments import highlight
 from pygments import lexers, styles
@@ -26,9 +27,12 @@ class MainWindow(QtWidgets.QMainWindow):
         stop:1 rgb(150, 150, 150));}
         ''')
 
+        # Highlight class
+        self.highlight = Highlight()
+
         # ComboBox
-        all_langs = [lexer[0] for lexer in lexers.get_all_lexers()]
-        all_styles = [style for style in styles.get_all_styles()]
+        all_langs = self.highlight.return_all_languages()
+        all_styles = self.highlight.return_all_styles()
         self.ui.comboBox.addItems(all_langs)
         self.ui.comboBox_2.addItems(all_styles)
 
@@ -42,51 +46,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.textBrowser.setHtml(code)
 
     def highlightEvent(self, code):
-        # Initial
-        self.kw_color = {}
-
-        style = styles.get_style_by_name(self.ui.comboBox_2.currentText())
-        class_style = HtmlFormatter(style=style).get_style_defs('.highlight')
-        print(class_style)
-
-        self.background = re.findall('\{ background: (#......;)', class_style)[0]
-        print(self.background)
-
-        class_style = re.findall('.highlight (\.\w+) \{ (.+) \}', class_style)
-        for cs in class_style:
-            key = 'class="{}"'.format(cs[0][1:])
-            style = 'style="{};"'.format(cs[1])
-
-            self.kw_color[key] = style
-
-        # Highlight
-        lines = code.split('\n')
-        results = ''
-        kw = set()
-
-        lexer = lexers.get_lexer_by_name(self.ui.comboBox.currentText())
-
-        for n in range(len(lines)):
-            space_n = 0
-            for w in lines[n]:
-                if w != ' ': break
-                space_n += 1
-
-            highlight_line = highlight(lines[n], lexer, HtmlFormatter())
-            new_line = re.findall('<span>.+', highlight_line)[0]
-            new_line = re.sub('<span></span>', '<span>{}</span>'.format('&nbsp;' * space_n), new_line)
-
-            for kc in self.kw_color:
-                new_line = re.sub(kc, self.kw_color[kc], new_line)
-            results += new_line
-
-            if n != len(lines) - 1:
-                results += '<br>\n'
-
-            for w in re.findall('class="\w+">', new_line):
-                kw.add(w)
-
-        results = '<div style="overflow: auto; background: {}; border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em .6em;">\n<pre style="margin: 0; line-height: 125%; white-space:nowrap">\n'.format(self.background) + results + '</pre></div>'
+        lexer = self.ui.comboBox.currentText()
+        style = self.ui.comboBox_2.currentText()
+        results = self.highlight.highlightEvent(code, style, lexer)
 
         return results
 
--- web app/main.py
+++ web app/main.py
@@ -2,10 +2,13 @@
 """
 Using Python to process the pygments
 """
-
 from flask import Flask, render_template, request
+from highlight import Highlight
 
 app = Flask(__name__)
+highlight = Highlight()
+print(highlight)
+
 
 
 @app.route('/')
@@ -17,7 +20,9 @@ def index():
 def success():
     if request.method == 'POST':
         code = request.form['source code']
-        return render_template('code2html.html', html=code, iframe=iframe)
+        lang = request.form['lang']
+        print(request)
+        return render_template('code2html.html', html=code, iframe=lang)
 
 
 if __name__ == '__main__':
